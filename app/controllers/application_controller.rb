95ddd29724380d0c47942d61d1a3437c1bb63854
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-29 18:51:44 +0400

    Senitize user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    respond_to :json, :html
+
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 end
