3c0571c4b7cdc25cab93284ca29b43d3960c9bf6
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-30 10:03:22 +0400

    Namespce task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root "goals#index"
   devise_for :users
+  resources :goals do 
+    resources :tasks
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
